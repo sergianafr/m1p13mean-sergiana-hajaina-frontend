91f8bfdbdd1ed249c7b08be8dbc3ec4d91660a02
Bedouu <155775889+Bedouu@users.noreply.github.com>
2026-02-12 12:22:33 +0300

    Material Modul
    création d'un module spécial Angular Material pour garder un code clean
    USE: importer juste que MaterialModule et dans ce dernier tous les imports de Ang Mat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
+import { MaterialModule } from './material/material.module';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
