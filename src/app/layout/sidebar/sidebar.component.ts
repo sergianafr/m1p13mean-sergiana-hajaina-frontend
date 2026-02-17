97e55bb86fbdf9d5f270e6a416c0ebac0d9bc48f
Bedouu <155775889+Bedouu@users.noreply.github.com>
2026-02-17 22:41:13 +0300

    exemple

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -20,7 +20,9 @@ export class SidebarComponent {
   readonly navigate = output<void>();
 
   protected readonly navItems: NavItem[] = [
-    { label: 'Home', icon: 'home', link: '/home' }
+    { label: 'Home', icon: 'home', link: '/home' },
+    { label: 'Profile', icon: 'person', link: '/profile' },
+    { label: 'Settings', icon: 'settings', link: '/settings' }
   ];
 
 }
