95f5c3fbd8d32212eaa555d8d76ca9c3b100ec87
Bedouu <155775889+Bedouu@users.noreply.github.com>
2026-02-25 02:33:22 +0300

    components

---
+++ src/app/features/box/pages/box-form/box-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './box-form.component.html',
+  styleUrl: './box-form.component.scss'
+})
+export class BoxFormComponent {
+
+}
---
+++ src/app/features/box/pages/box-list/box-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './box-list.component.html',
+  styleUrl: './box-list.component.scss'
+})
+export class BoxListComponent {
+
+}
