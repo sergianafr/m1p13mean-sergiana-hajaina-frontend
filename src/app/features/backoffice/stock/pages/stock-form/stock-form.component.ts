fd72bbee0a873f6c5d5bf0e70c0e600f058917e4
Bedouu <155775889+Bedouu@users.noreply.github.com>
2026-03-01 01:52:55 +0300

    stock component

---
+++ src/app/features/backoffice/stock/pages/stock-form/stock-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './stock-form.component.html',
+  styleUrl: './stock-form.component.scss'
+})
+export class StockFormComponent {
+
+}
---
+++ src/app/features/backoffice/stock/pages/stock-list/stock-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './stock-list.component.html',
+  styleUrl: './stock-list.component.scss'
+})
+export class StockListComponent {
+
+}
