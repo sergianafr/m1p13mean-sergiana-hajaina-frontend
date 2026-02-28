bb5f5dc273808c043b65e88b7e15dd2904af943b
Bedouu <155775889+Bedouu@users.noreply.github.com>
2026-02-28 23:36:08 +0300

    component frontoffice

---
+++ src/app/features/frontoffice/layout/shop-layout/shop-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './shop-layout.component.html',
+  styleUrl: './shop-layout.component.scss'
+})
+export class ShopLayoutComponent {
+
+}
---
+++ src/app/features/frontoffice/pages/details-produit/details-produit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-produit',
+  standalone: true,
+  imports: [],
+  templateUrl: './details-produit.component.html',
+  styleUrl: './details-produit.component.scss'
+})
+export class DetailsProduitComponent {
+
+}
