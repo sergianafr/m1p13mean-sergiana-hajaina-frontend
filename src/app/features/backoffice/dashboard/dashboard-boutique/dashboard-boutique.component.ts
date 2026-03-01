bc754ef5e3e06cd39fb92baf20cefd7e4ce3d936
Bedouu <155775889+Bedouu@users.noreply.github.com>
2026-03-02 02:37:15 +0300

    component

---
+++ src/app/features/backoffice/dashboard/dashboard-boutique/dashboard-boutique.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-boutique',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-boutique.component.html',
+  styleUrl: './dashboard-boutique.component.scss'
+})
+export class DashboardBoutiqueComponent {
+
+}
